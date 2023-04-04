412216a2aa1bff2d580e25da22279bd4c08ec259
Hyeongwoo Moon <moc9058@gmail.com>
2023-04-04 12:58:42 +0900

    added some words

--- word_gen.py
+++ word_gen.py
@@ -12,14 +12,24 @@ except:
     
 prob_index, ans_index = 0,1
 words = words_file.readlines()
-
-
+num_cycle = -1
+wrong_cycles = []
+wrong_words = []
 while True:
+    num_cycle += 1
     num_words = len(words)
-    if num_words == 0:
+    try:
+        random_index = random.randint(0, num_words-1)
+    except:
         break
-    random_index = random.randint(1, num_words)-1
     word = words[random_index][:-1]
+    try:
+        if num_words == 0 or num_cycle == wrong_cycles[0]:
+            wrong_cycles.pop(0)
+            word = wrong_words.pop(0)
+    except:
+        pass
+
     word_splitted = word.split("；")
     korean = word_splitted[0]
     print(f"{korean}({num_words} left)", end=" ")
@@ -32,10 +42,15 @@ while True:
         to_print += f", {word_splitted[2]}그룹"
     print(to_print)
 
+    
     a = input()
     if a.lower() == 'x':
         break
     elif a.lower() == 'o':
         words.pop(random_index)
+    else:
+        wrong_cycles.append(num_cycle + 5)
+        wrong_words.append(word)
+
     print()
 words_file.close()
